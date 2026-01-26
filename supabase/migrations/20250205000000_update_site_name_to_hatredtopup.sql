94dc9fbea154003548611690d51f825bf15d7a64
Stanly Zabala <stanylzabala@gmail.com>
2026-01-27 01:19:11 +0800

    Remove migration that changes branding to hatredtopup - keeping Diginix branding

--- supabase/migrations/20250205000000_update_site_name_to_hatredtopup.sql
+++
@@ -1,9 +0,0 @@
--- Update site name and description to new branding
-UPDATE site_settings
-SET value = 'hatredtopup'
-WHERE id = 'site_name';
-
--- Clear site description per new requirements
-UPDATE site_settings
-SET value = ''
-WHERE id = 'site_description';
